4d0445a0f061699112cde77225616de9d0941cd0
Bandan Kumar Mahto <bandan.pixster@gmail.com>
2026-05-20 14:11:43 +0530

    Initial Commit

---
+++ StringTranslatorDemo/StringTranslatorDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  StringTranslatorDemoApp.swift
+//  StringTranslatorDemo
+//
+//  Created by iOS Department on 20/05/26.
+//
+
+import SwiftUI
+
+@main
+struct StringTranslatorDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
